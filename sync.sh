0af202873445485f7f4210149a5419b8b9846e5c
Carl <cduffy@snu.ac.kr>
2024-05-01 08:17:59 +0900

    Latest cleanups.

--- sync.sh
+++
@@ -1,3 +0,0 @@
-rsync --exclude='*log*' -r -e ssh . virt@192.168.123.41:/home/virt/nvmevirt
-ssh virt@192.168.123.41 "make -C /home/virt/nvmevirt clean && make -j -C /home/virt/nvmevirt"
-scp virt@192.168.123.41:/home/virt/nvmevirt/nvmev.ko /home/move/kerneldebug/
