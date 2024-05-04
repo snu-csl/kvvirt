ba0b89bd74c389ac7d333163f8dd6a8721d2ea40
Carl <cduffy@snu.ac.kr>
2024-05-04 13:47:29 +0900

    No need for this anymore.

--- io.c
+++ io.c
@@ -101,7 +101,6 @@ static unsigned int __do_perform_io_kv(int sqid, int sq_entry)
             real_vlen = *(uint32_t*) (ptr + 
                     sizeof(uint8_t) + klen);
             NVMEV_ASSERT(real_vlen > 0);
-            NVMEV_ASSERT(real_vlen < 1024);
             length = real_vlen + sizeof(uint32_t);
         }
     } else if(write) {
@@ -305,7 +304,6 @@ static unsigned int __do_perform_io_kv(int sqid, int sq_entry)
         //    //NVMEV_INFO("Returning length %u in io_kv\n", real_vlen);
         //}
         NVMEV_ASSERT(real_vlen > 0); 
-        NVMEV_ASSERT(real_vlen < 1024);
         return real_vlen;
     } else if(write || orig_len == 0) {
         ptr = (void*) cmd->kv_store.rsvd;
